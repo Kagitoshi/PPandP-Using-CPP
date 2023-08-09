4e39f4cbe677b7d42f6a6b584b2f28fc1de87715
Kagitoshi <santocomet@hotmail.com>
2023-08-09 14:42:55 +0900

    Chapter 2 - Exercise 1

---
+++ Chapter 2/Exercises/1/main.cpp
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Hello, World!\n"; // outputs "Hello, World!"
+
+    cout << "Here, we go!\n"; // outputs "Here, we go!"
+
+    keep_window_open(); // keeps window open until a key is entered.
+
+    return 0;
+}
