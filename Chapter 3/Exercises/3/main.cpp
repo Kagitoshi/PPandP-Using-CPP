d56883600fb9c9c287d0daeaf9482a0d9159b359
Kagitoshi <santocomet@hotmail.com>
2023-08-15 16:23:58 +0900

    Chapter 3 - Exercise 3 - A non-functional program, just to see errors from the compiler/IDE.

---
+++ Chapter 3/Exercises/3/main.cpp
@@ -0,0 +1,22 @@
+// full of errors with names to see how the compiler reacts
+#include "std_lib_facilities.h"
+
+int main()
+{
+    int x {2.3};
+    int y {2};
+    int X {2};
+    int y {3};
+    int _z {4};
+    int 6 {6};
+    int 7seven {7};
+
+    double double {1.0};
+    double int {2.0};
+    Double a {3.0};
+    double b {4.5};
+    
+
+
+    return 0;
+}
