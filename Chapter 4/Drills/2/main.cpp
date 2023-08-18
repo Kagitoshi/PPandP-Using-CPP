8699df6f559592977bffed41bf1ad5a2aff9fda0
Kagitoshi <santocomet@hotmail.com>
2023-08-18 14:43:11 +0900

    Chapter 2 - Drill 2 - Compare 2 integers, display smaller then larger number.

---
+++ Chapter 4/Drills/2/main.cpp
@@ -0,0 +1,45 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    string userNum1{};
+    string userNum2{};
+
+    while(true) {
+        cout << "Please enter your first integer (type '|' if you wish to quit): ";
+        cin >> userNum1;
+        if (userNum1 == "|")
+        {
+            break;
+        }
+        else
+        {
+        }
+
+        cout << "Please enter your second integer(type '|' if you wish to quit): ";
+        cin >> userNum2;
+        if (userNum2 == "|")
+        {
+            break;
+        }
+        else
+        {
+        }
+
+        if (stoi(userNum1) <stoi(userNum2))
+        {
+            cout << "You have entered " << stoi(userNum1) << ", which is the smaller of the two, & "
+                 << stoi(userNum2) << ", which is larger.\n";
+        }
+        else
+        {
+            cout << "You have entered " << stoi(userNum2) << ", which is the smaller of the two & "
+                 << stoi(userNum1) << ", which is larger.\n";
+        }
+
+    }
+
+    cout << "\nThanks for using \"Enter 2 Integers\". See you next time!";
+
+    return 0;
+}
