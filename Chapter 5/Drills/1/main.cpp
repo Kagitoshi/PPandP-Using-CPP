8bfa1cb613e5ba4b9b1341638366e39b0ba2335e
Kagitoshi <santocomet@hotmail.com>
2023-10-12 14:45:34 +0900

    Chapter 5 - Drill 1 - Simple Calculator

--- Chapter 5/Drills/1/main.cpp
+++ Chapter 5/Drills/1/main.cpp
@@ -1,47 +1,213 @@
+
+//
+// This is example code from Chapter 6.7 "Trying the second version" of
+// "Software - Principles and Practice using C++" by Bjarne Stroustrup
+//
+
+/*
+    This file is known as calculator02buggy.cpp
+
+    I have inserted 5 errors that should cause this not to compile
+    I have inserted 3 logic errors that should cause the program to give wrong results
+
+    First try to find and remove the bugs without looking in the book.
+    If that gets tedious, compare the code to that in the book (or posted source code)
+
+    Happy hunting!
+
+*/
+
 #include "std_lib_facilities.h"
+
+//------------------------------------------------------------------------------
+
+class Token
+{
+public:
+    char kind;        // what kind of token
+    double value;     // for numbers: a value
+
+    Token(char ch)    // make a Token from a char
+        :kind(ch), value(0) { }
+
+    Token(char ch, double val)     // make a Token from a char and a double
+        :kind(ch), value(val) { }
+};
+
+//------------------------------------------------------------------------------
+
+class Token_stream {
+public:
+    Token get();      // get a Token (get() is defined elsewhere)
+    void putback(Token t);    // put a Token back
+private:
+    bool full{false};        // is there a Token in the buffer?
+    Token buffer{'0'};     // here is where we keep a Token put back using putback()
+};
+
+//------------------------------------------------------------------------------
+
+// The putback() member function puts its argument back into the Token_stream's buffer:
+void Token_stream::putback(Token t)
+{
+if (full) error("putback() into a full buffer");
+buffer = t;       // copy t to buffer
+full = true;      // buffer is now full
+}
+
+//------------------------------------------------------------------------------
+
+Token Token_stream::get()
+{
+    if (full) {       // do we already have a Token ready?
+        // remove token from buffer
+        full = false;
+        return buffer;
+    }
+
+    char ch;
+    cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
+
+    switch (ch) {
+        case '=':    // for "print"
+        case 'x':    // for "quit"
+        case '(': case ')': case '+': case '-': case '*': case '/':
+            return Token{ch};        // let each character represent itself
+        case '.':
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+        {
+            cin.putback(ch);         // put digit back into the input stream
+            double val;
+            cin >> val;              // read a floating-point number
+            return Token{'8', val};   // let '8' represent "a number"
+        }
+        default:
+            error("Bad token");
+    }
+}
+
+//------------------------------------------------------------------------------
+
+Token_stream ts;        // provides get() and putback() 
+
+//------------------------------------------------------------------------------
+
+double expression();    // declaration so that primary() can call expression()
+
+//------------------------------------------------------------------------------
+
+// deal with numbers and parentheses
+double primary()
+{
+    Token t = ts.get();
+    switch (t.kind) {
+        case '(':    // handle '(' expression ')'
+        {
+            double d = expression();
+            t = ts.get();
+            if (t.kind != ')') error("')' expected");
+            return d;
+        }
+        case '8':            // we use '8' to represent a number
+            return t.value;  // return the number's value
+        default:
+            error("primary expected");
+    }
+}
+
+//------------------------------------------------------------------------------
+
+// deal with *, /, and %
+double term()
+{
+    double left = primary();
+    Token t = ts.get();        // get the next token from token stream
+
+    while (true) {
+        switch (t.kind) {
+            case '*':
+                left *= primary();
+                t = ts.get();
+                break;
+            case '/':
+            {
+                double d = primary();
+                if (d == 0) error("divide by zero");
+                left /= d;
+                t = ts.get();
+                break;
+            }
+            default:
+                ts.putback(t);     // put t back into the token stream
+                return left;
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+// deal with + and -
+double expression()
+{
+    double left = term();      // read and evaluate a Term
+    Token t = ts.get();        // get the next token from token stream
+
+    while (true) {
+        switch (t.kind) {
+            case '+':
+                left += term();    // evaluate Term and add
+                t = ts.get();
+                break;
+            case '-':
+                left -= term();    // evaluate Term and subtract
+                t = ts.get();
+                break;
+            default:
+                ts.putback(t);     // put t back into the token stream
+                return left;       // finally: no more + or -: return the answer
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
 
 int main()
 try
 {
-//  cout << "Success!\n";
-//  cout << "Success!\n";
-//  cout << "Success!\n";
-//  cout << "Success!\n";
-//  int res = 7; vector<int> v(10); v[5] = res; cout << "Success!\n";
-//  vector<int> v(10); v[5] = 7; if (v[5]=7) cout << "Success!\n";
-//  bool cond{true}; if (cond) cout << "Success!\n"; else cout << "Fail!\n";
-//  bool c = false; if (!c) cout << "Success!\n"; else cout << "Fail!\n";
-//  string s = "ape"; bool c = "fool" < s; if (!c) cout << "Success!\n";
-//  string s = "ape"; if (s!="fool") cout << "Success!\n";
-//  string s = "ape"; if (s!="fool") cout << "Success!\n";
-//  string s = "ape"; if (s!="fool") cout << "Success!\n";
-//  vector<char> v(5); for (int i=0; i<v.size(); ++i) ; cout << "Success!\n";
-//  vector<char> v(5); for (int i=0; i<v.size(); ++i) ; cout << "Success!\n";
-//  string s = "Success!\n"; for (int i=0; i<8; ++i) cout << s[i]; cout << '\n';
-//  if (true)  cout << "Success!\n"; else cout << "Fail!\n";
-//  int x = 64; char c = x; if (c==64) cout << "Success!\n";
-//  string s = "Success!\n"; for (int i=0; i<10; ++i) cout << s[i];
-//  vector<int> v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
-//  int i=0; int j = 9; while (i<10) ++i; if (j<i) cout << "Success!\n";
-//  int x = 4; double d = 17.0/(x - 2.0); if (d==2*x+0.5) cout << "Success!\n";
-//  string s = "Success!\n"; for (int i=0; i < 8; ++i) cout << s[i]; cout << '\n';
-//  int i=0; while (i<10) ++i; int j{5}; if (j<i) cout << "Success!\n";
-//  int x = 4; double d = 17.0/(x - 2.0); if (d==2*x+0.5) cout << "Success!\n";
-  cout << "Success!\n";
+    cout << "Welcome to our simple calculator.\n"
+            "Please enter expressions using floating-point numbers.\n"
+            "Please use +, -, *, and/or / to calculate your problems.\n"
+            "Place an equal sign '=' at the end of your problem and press enter to recieve the answer.\n"
+            "You may also use parentheses to specify which part of the solution\n"
+            "you wish to calculate first.\n"
+            "e.g: 1*(2+3)=\n\n"
+            "When you wish to quit, please enter 'x'\n\n";
 
+    double val{0};
+    while (cin) {
+        Token t = ts.get();
+
+        if (t.kind == 'x') break; // 'x' for quit
+        if (t.kind == '=')        // '=' for "print now"
+            cout << "=" << val << '\n';
+        else
+            ts.putback(t);
+        val = expression();
+    }
+
+    cout << "Thank you for using our simple calculator.\n";
     keep_window_open();
-    return 0;
 }
-
-catch (exception& e)
-{
+catch (exception& e) {
     cerr << "error: " << e.what() << '\n';
     keep_window_open();
     return 1;
 }
-catch (...)
-{
+catch (...) {
     cerr << "Oops: unknown exception!\n";
     keep_window_open();
     return 2;
 }
+
+//------------------------------------------------------------------------------
